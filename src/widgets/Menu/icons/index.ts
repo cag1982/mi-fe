db4fa38bc1af7037611e6288ee024be2b248b4e9
Dev A <cag1982@protonmail.com>
2021-06-17 12:45:57 -0300

    referral icon + minor font-color fixes

--- src/widgets/Menu/icons/index.ts
+++ src/widgets/Menu/icons/index.ts
@@ -1,4 +1,5 @@
 export { default as FarmIcon } from "./Farm";
+export { default as ReferralIcon } from "./Referral";
 export { default as GroupsIcon } from "./Groups";
 export { default as HamburgerIcon } from "./Hamburger";
 export { default as HamburgerCloseIcon } from "./HamburgerClose";
